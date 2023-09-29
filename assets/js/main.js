c1efd1ba49772f2314a90408c205185121c2ae70
freetimezp <kraftukrnet@ukr.net>
2023-09-29 22:17:33 +0300

    05. js-e-commerce-05-jbl. Blur header on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,12 @@ const linkAction = () => {
 navLink.forEach(el => el.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
+}
 
+window.addEventListener('scroll', blurHeader);
 
 /*=============== SWIPER FAVORITES ===============*/
 
