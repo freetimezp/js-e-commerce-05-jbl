695d5b26d58a4fe1e355eddc62020e08e0a53ba3
freetimezp <kraftukrnet@ukr.net>
2023-10-01 20:04:44 +0300

    10. js-e-commerce-05-jbl. Scrollbar. Show scrollup btn.

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,12 @@ let swiperFavorite = new Swiper('.favorite__swiper', {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
 
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
