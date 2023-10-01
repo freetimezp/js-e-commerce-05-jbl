01f059f2f3ab898afba542e2d53da3b472c2658f
freetimezp <kraftukrnet@ukr.net>
2023-10-01 20:28:36 +0300

    11. js-e-commerce-05-jbl. Set menu active on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,24 @@ const scrollUp = () => {
 window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
 
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
+        } else {
+            sectionsClass.classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
